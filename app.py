f3655a221266daf6061b3528abf818d03a108b88
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-05-20 12:51:39 +0300

    Update

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import gradio as gr
-from lexenlem.preprocessing.vabamorf import VabamorfAdHocProcessor
+from lexenlem.preprocessing.adhoc import AdHocLemmatizer
 
-lemmatizer = VabamorfAdHocProcessor(path="et_edt_lemmatizer.pt")
+lemmatizer = AdHocLemmatizer(path="vb_stanza_no_compound_no_deriv.pt", use_stanza=True)
 
 gradio_ui = gr.Interface(
-    fn=lemmatizer.lemmatize,
+    fn=lemmatizer.__call__,
     title="Lemmatization",
     description="Enter a text to lemmatize",
     inputs=[
-        gr.inputs.Textbox(lines=7, label="Paste a text here"),
+        gr.inputs.Textbox(lines=7, label="Enter or paste a text here"),
     ],
     outputs=[
         gr.outputs.Textbox()
     ],
-    examples=["""
-    Andmebaasid võimaldavad endas hoida struktureeritud andmeid ja nendest on väga lihtne neid kätte saada rakenduste jaoks. Andmebaasid on tihti vajalikud erinevate veebirakenduste jaoks. MySQL on üks kahest andmebaasi mootorist mida meie pakume. Teiseks on PostgreSQL.
+    examples=["""Andmebaasid võimaldavad endas hoida struktureeritud andmeid ja nendest on väga lihtne neid kätte saada rakenduste jaoks.
     """],
-    enable_queue=True,
     allow_screenshot=False,
     allow_flagging="never",
 )
 
-gradio_ui.launch(debug=True)
+gradio_ui.launch(debug=True, enable_queue=True)
