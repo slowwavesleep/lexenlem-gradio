2baa8ec36e08ff26e2f6ed320ae9e44b587537f8
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-05-24 15:06:07 +0300

    Add models etc

---
+++ README.md
@@ -0,0 +1,9 @@
+---
+title: Lexicon-enhanced lemmatization for Estonian
+colorFrom: black
+colorTo: blue
+sdk: gradio
+sdk_version: 2.9.0
+app_file: app.py
+pinned: false
+---
--- app.py
+++ app.py
@@ -1,22 +1,63 @@
+from typing import Dict, List
+
 import gradio as gr
 from lexenlem.preprocessing.adhoc import AdHocLemmatizer
 
-lemmatizer = AdHocLemmatizer(path="vb_stanza_no_compound_no_deriv.pt", use_stanza=True)
+models: Dict[str, AdHocLemmatizer] = {
+    "Lemmatize": AdHocLemmatizer(path="vb_stanza_no_compound_no_deriv.pt", use_stanza=True),
+    "Lemmatize with special symbols": AdHocLemmatizer(
+        path="vb_stanza_symbols.pt", use_stanza=True, allow_compound_separator=True, allow_derivation_sign=True
+    )
+}
+
+
+def predict(text: str, model_name: str) -> List[str]:
+    if model_name not in models:
+        raise RuntimeError("Unknown model")
+    return models[model_name](text)
+
 
 gradio_ui = gr.Interface(
-    fn=lemmatizer.__call__,
-    title="Lemmatization",
-    description="Enter a text to lemmatize",
+    fn=predict,
+    title="Lexicon-enhanced lemmatization for Estonian",
+    description="The purpose of this demo is to demonstrate the results of cite paper. The idea is to utilize the"
+                " input of an external resource (a `lexicon` — Vabamorf morphological analyzer in this particular case)"
+                " as an additional input to improve the results of a neural lemmatizer model. Said additional input"
+                " is a concatenation of one or more lemma candidates provided by Vabamorf. Morphological features and"
+                " the part of speech are provided by Stanza in this demo, although it's possible to use native Vabamorf"
+                " features as well (the results, however, are going to be slightly worse).\n\n"
+                " The lexicon-enhanced lemmatizer itself is based on an older version of Stanza. The models were"
+                " trained on the Estonian Dependency Treebank version 2.7.\n\n"
+                " Two variants of lemmatization are provided in the demo: regular lemmatization and lemmatization with"
+                " special symbols, which are `=` and `_`, denoting morphological derivation and separating parts of"
+                " compound words respectively. The latter was trained on the original data with Vabamorf set to output"
+                " these special symbols, while the latter was trained with `=` and `_` removed from the data and"
+                " vabamorf output .",
     inputs=[
-        gr.inputs.Textbox(lines=7, label="Enter or paste a text here"),
+        gr.inputs.Textbox(lines=7, label="Input text in the box below", placeholder="Text to lemmatize"),
+        gr.inputs.Radio(list(models.keys()), label="Lemmatization type")
     ],
     outputs=[
         gr.outputs.Textbox()
     ],
-    examples=["""Andmebaasid võimaldavad endas hoida struktureeritud andmeid ja nendest on väga lihtne neid kätte saada rakenduste jaoks.
-    """],
+    examples=[
+        [
+            "Andmebaasid võimaldavad endas hoida struktureeritud andmeid ja nendest on väga lihtne neid kätte saada "
+            "rakenduste jaoks.",
+            "Lemmatize"
+        ],
+        [
+            "Aga äärmused sünnitavad äärmusi ja meesshovinistid tõstavad omakorda upsakalt päid ja muid kehaosi.",
+            "Lemmatize with special symbols"
+        ],
+        [
+            "Juunioride MMilt pronksmedali teeninud tallinlane Eivinas Guzas seekord veel meeste koondisse ei mahtunud.",
+            "Lemmatize"
+        ]
+    ],
     allow_screenshot=False,
     allow_flagging="never",
 )
 
-gradio_ui.launch(debug=True, enable_queue=True)
+
+gradio_ui.launch(debug=False, enable_queue=True)
